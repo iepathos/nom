072e88c4eeee1cd89a4511ef7eccf1963157fd77
Glen Baker <iepathos@gmail.com>
2018-11-25 17:53:31 -0800

    fix docker install on ubuntu

--- docker/install.sh
+++ docker/install.sh
@@ -11,6 +11,10 @@ elif [[ "$arch" = "Linux" ]]; then
     curl \
     software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
     sudo apt-get update -y
     sudo apt-get install docker-ce -y
 fi
