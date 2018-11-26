aaadb78525638e3b01a70d337851105886606fb0
Glen Baker <iepathos@gmail.com>
2018-11-25 23:43:43 -0800

    fix nopasswd install for osx

--- nopasswd/install.sh
+++ nopasswd/install.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# fixme: this has trouble in osx
-arch=$(uname)
-if [[ "$arch" = "Darwin" ]]; then
-	echo "nopasswd script has trouble in osx, skipping for the moment"
-elif [[ "$arch" = "Linux" ]]; then
-	if ! sudo grep -Fxq "$USER ALL=(ALL) NOPASSWD: ALL" /etc/sudoers; then
-		sudo echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-	else
-		echo "$USER nopasswd entry already in /etc/sudoers"
-	fi
+if ! sudo grep -Fxq "$USER ALL=(ALL) NOPASSWD: ALL" /etc/sudoers; then
+	echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers
+else
+	echo "$USER nopasswd entry already in /etc/sudoers"
 fi
