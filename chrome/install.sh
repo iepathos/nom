a14c07c8908d4fd2a17e9f5920a140c0478bb239
Glen Baker <glen@headspin.io>
2018-12-08 20:01:02 -0800

    cleanup

--- chrome/install.sh
+++ chrome/install.sh
@@ -4,6 +4,7 @@ arch=$(uname)
 if [[ "$arch" = "Darwin" ]]; then
 	brew cask install google-chrome
 elif [[ "$arch" = "Linux" ]]; then
-	curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o google-chrome-stable_current_amd64.deb
-	sudo dpkg -i google-chrome-stable_current_amd64.deb
+	curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o /tmp/google-chrome-stable_current_amd64.deb
+	sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
+	rm /tmp/google-chrome-stable_current_amd64.deb
 fi
