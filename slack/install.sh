93d822768aeaa292fdd9689b3d2ad132ce0d183a
Glen Baker <glen@headspin.io>
2018-12-08 20:04:15 -0800

    cleanup

--- slack/install.sh
+++ slack/install.sh
@@ -6,4 +6,5 @@ if [[ "$arch" = "Darwin" ]]; then
 elif [[ "$arch" = "Linux" ]]; then
 	curl "https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.3-amd64.deb" --output /tmp/slack-desktop-3.3.3-amd64.deb
 	sudo dpkg -i /tmp/slack-desktop-3.3.3-amd64.deb
+	rm /tmp/slack-desktop-3.3.3-amd64.deb
 fi
