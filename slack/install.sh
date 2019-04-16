87373f4bfa7c4e7ee7f4f3164e67ea390136f468
Glen Baker <iepathos@gmail.com>
2019-04-15 23:56:24 -0700

    update slack install to use snap

--- slack/install.sh
+++ slack/install.sh
@@ -4,7 +4,5 @@ arch=$(uname)
 if [[ "$arch" = "Darwin" ]]; then
 	brew cask install slack
 elif [[ "$arch" = "Linux" ]]; then
-	curl "https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.3-amd64.deb" --output /tmp/slack-desktop-3.3.3-amd64.deb
-	sudo dpkg -i /tmp/slack-desktop-3.3.3-amd64.deb
-	rm /tmp/slack-desktop-3.3.3-amd64.deb
+	sudo snap install slack --classic
 fi
